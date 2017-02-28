6fd6bfd12dda87f48cff7c67cbbd62c0187b247e
Leonard Goff <leonard.goff@columbia.edu>
2017-02-27 19:16:18 -0500

    Feb 27 2017 deployment

--- R/rdbounds.R
+++ R/rdbounds.R
@@ -1,5 +1,23 @@
+#--------------------------------------------------------------------------
 #VERSION 0.95 - BETA
+#DISCLAIMER: THIS IS A BETA VERSION OF THE CODE AND IS STILL UNDERGOING TESTING.
+#WE APPRECIATE YOUR COMMENTS AND ANY ISSUES NOTICED, AT LEONARD.GOFF@COLUMBIA.EDU
+#Copyright (C) 2017 Francois Gerard, Leonard Goff, Miikka Rokkanen, & Christoph Rothe.
 
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#--------------------------------------------------------------------------
 #install.packages("data.table")
 #library(data.table)
 #install.packages("formattable")
--- README.md
+++
@@ -1,2 +0,0 @@
-## Synopsis
-This is a public repository for the R package rdbounds, implementing the estimation procedure developed in [Bounds on Treatment Effects in Regression Discontinuity Designs under Manipulation of the Running Variable, with an Application to Unemployment Insurance in Brazil](http://www.nber.org/papers/w22892, "NBER Working Paper"), by François Gerard, Miikka Rokkanen, and Christoph Rothe. See rdbounds.pdf for details.
